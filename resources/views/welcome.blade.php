de60b7919be1b181ca319d7e0591f3adc7d7aeb7
FeliceDArcangelo <felicedarcangelo25@gmail.com>
2023-06-27 07:18:25 +0200

    template

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        @vite('resources/js/app.js')
+    </head>
+
+    {{-- <img src="{{ Vite::asset ('resources/img/logo.png') }}" alt="">     come chiamare un'immagine--}}
+
+    <body>
+        <h1></h1>
+    </body>
+</html>
